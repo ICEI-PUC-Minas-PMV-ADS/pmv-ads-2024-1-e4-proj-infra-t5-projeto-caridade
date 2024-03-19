9b499b3a25808f2171d059a966bcd1fda4256f2a
Lucas Russo <lucasrusso0208@gmail.com>
2024-03-18 21:55:15 -0300

    feat: adding EF package

--- src/backend/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
+++ src/backend/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08a29f5b02fd117a2cdc67b1b9e54c50b503907")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08dd4f93c63c6840ec506021e20199f4dd88a281")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
