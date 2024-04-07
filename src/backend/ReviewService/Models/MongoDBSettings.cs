e81244bee4d11c617fd52b487a349bb843d0b780
Lucas Russo <lucasrusso0208@gmail.com>
2024-04-06 21:30:55 -0300

    feat: initial configuration of mongodb on docker compose

---
+++ src/backend/ReviewService/Models/MongoDBSettings.cs
@@ -0,0 +1,8 @@
+namespace MongoSettings.Models;
+
+public class MongoDBSettigs
+{
+    public string ConnectionURI { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string CollectionName { get; set; } = null!;
+}
