05ce812d5f66137a646fdcb4f38b0d7727904e5c
Lucas Russo <lucasrusso0208@gmail.com>
2024-05-04 14:16:30 -0300

    fix: fixing home screen

--- src/frontendWEB/src/pages/Home/Home.tsx
+++ src/frontendWEB/src/pages/Home/Home.tsx
@@ -1,41 +1,33 @@
+import { Box, Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import charityImage from "../../assets/charity.jpeg";
 
 function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "left",
-        gap: "40px",
-        padding: "60px",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          gap: "10px",
-        }}
-      >
-        <h1>Encontre projetos que te engajam!</h1>
-        <span>
-          Buscador de projetos de caridade que tem
-          <br /> como objetivo auxiliar na divulgação e busca de informações{" "}
-          <br />
-          relativas a instituições filantrópicas em âmbito global.
-        </span>
-        <Link to="/register">
-          <button>Criar conta</button>
-        </Link>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-      </div>
-      <div>
-        <img src="" alt="teste" />
-      </div>
-    </div>
+    <Box display="flex">
+      <Box height="100%">
+        <Typography variant="h4" color="#004e63">
+          Encontre projetos que te engajam!
+        </Typography>
+        <Typography variant="h6" mt={2}>
+          Buscador de projetos de caridade que tem como objetivo auxiliar na
+          divulgação e busca de informações relativas a instituições
+          filantrópicas em âmbito global.
+        </Typography>
+        <Box mt={3}>
+          <Link to="/register">
+            <Button variant="contained">Criar conta</Button>
+          </Link>
+        </Box>
+      </Box>
+      <Box>
+        <img
+          src={charityImage}
+          alt="Charity Hands"
+          style={{ borderRadius: "10px" }}
+        />
+      </Box>
+    </Box>
   );
 }
 
