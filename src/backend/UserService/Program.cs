d64752753de0ab6a5fac0c21da7af0630e3284ce
Felipe Ferreira <114882542+Felipeadf@users.noreply.github.com>
2024-05-04 18:14:02 -0300

    Bug de Userservice

--- src/backend/UserService/Program.cs
+++ src/backend/UserService/Program.cs
@@ -9,6 +9,7 @@ var services = builder.Services;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 services.AddAuthorization();
 
