ed5ac0698c14dee44730e0cd46cefe1e9660830c
Vinicius Carneiro Gonçalves <105397334+viniciuscg@users.noreply.github.com>
2024-05-04 23:12:40 -0300

    commit with .env now

--- src/frontendWEB/src/services/charityApi/CharityService.ts
+++ src/frontendWEB/src/services/charityApi/CharityService.ts
@@ -9,17 +9,17 @@ let config = {
 
 export class CharityServices{
   static async getAllProjectIds() {
-    const response = await apiCharity.get("/active/ids?api_key=1727baac-3269-4c64-ba2a-2471ec20e3ba", config)
+    const response = await apiCharity.get(`/active/ids?api_key=${process.env.API_KEY}`, config)
     return response.data
   }
 
   static async getById(id: number) {
-    const response = await apiCharity.get(`projects/collection/ids?api_key=1727baac-3269-4c64-ba2a-2471ec20e3ba&projectIds=${id}`, config)      
+    const response = await apiCharity.get(`projects/collection/ids?api_key=${process.env.API_KEY}&projectIds=${id}`, config)      
     return response.data
   }
 
   static async getProjectsByIds(ids: number[]) {
-    const response = await apiCharity.get(`projects/collection/ids?api_key=1727baac-3269-4c64-ba2a-2471ec20e3ba&projectIds=
+    const response = await apiCharity.get(`projects/collection/ids?api_key=${process.env.API_KEY}&projectIds=
       ${ids[0]},${ids[1]},${ids[2]},${ids[3]},${ids[4]},${ids[5]},${ids[6]},${ids[7]},${ids[8]},${ids[9]}`, config
     )
     return response.data
