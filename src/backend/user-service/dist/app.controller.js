834e2f734de5a5e7c5c82a4b1719de3a4edc0166
Vinicius Carneiro Gonçalves <105397334+viniciuscg@users.noreply.github.com>
2024-05-10 04:17:15 -0300

    adc prisma com User  model

--- src/backend/user-service/dist/app.controller.js
+++ src/backend/user-service/dist/app.controller.js
@@ -20,9 +20,6 @@ let AppController = class AppController {
     getHello() {
         return this.appService.getHello();
     }
-    getAllUsers() {
-        return this.appService.getHello();
-    }
 };
 exports.AppController = AppController;
 __decorate([
@@ -32,13 +29,6 @@ __decorate([
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", String)
 ], AppController.prototype, "getHello", null);
-__decorate([
-    (0, common_1.Post)(),
-    (0, microservices_1.MessagePattern)({ cmd: 'HELLO_USER_ALL' }),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", []),
-    __metadata("design:returntype", String)
-], AppController.prototype, "getAllUsers", null);
 exports.AppController = AppController = __decorate([
     (0, common_1.Controller)(),
     __metadata("design:paramtypes", [app_service_1.AppService])
