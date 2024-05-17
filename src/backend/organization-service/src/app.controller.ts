06820e69e318458dfd337b9577e7596271bed2d0
Lucas Russo <lucasrusso0208@gmail.com>
2024-05-16 23:00:00 -0300

    creating main routes (organization service)

--- src/backend/organization-service/src/app.controller.ts
+++ src/backend/organization-service/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessagePattern } from '@nestjs/microservices';
 
@@ -10,5 +10,23 @@ export class AppController {
   @MessagePattern({ cmd: 'HELLO_ORGANIZATION' })
   async getHello(): Promise<string> {
     return await this.appService.getHello();
+  }
+
+  @Get('/get-all')
+  @MessagePattern({ cmd: 'GET_ALL_ORGANIZATIONS' })
+  getAll() {
+    return this.appService.getAll();
+  }
+
+  @Get('/get-by-id/:id')
+  @MessagePattern({ cmd: 'GET_BY_ID_ORGANIZATIONS' })
+  async getById(@Param() { id }: any) {
+    return this.appService.getById(id);
+  }
+
+  @Get('/get-by-country/:countryCode')
+  @MessagePattern({ cmd: 'GET_BY_COUNTRY_ORGANIZATIONS' })
+  async getByCountry(@Param() { countryCode }: any) {
+    return this.appService.getByCountry(countryCode);
   }
 }
--- src/backend/organization-service/src/app.service.ts
+++ src/backend/organization-service/src/app.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import api from './api_service';
+import { organizations } from './organizations-cache';
 
 @Injectable()
 export class AppService {
+  getAll() {
+    return organizations;
+  }
+
+  async getById(id: string) {
+    try {
+      const response = await api.get(
+        `/api/public/orgservice/organization/${id}?api_key=a6d2b676-00e7-47de-9d87-ccec8b424104`,
+      );
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching data:', error.data);
+      throw error;
+    }
+  }
+
+  async getByCountry(countryCode: string) {
+    try {
+      const response = await api.get(
+        `/api/public/projectservice/countries/${countryCode}/projects?api_key=a6d2b676-00e7-47de-9d87-ccec8b424104`,
+      );
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching data:', error.data);
+      throw error;
+    }
+  }
+
   async getHello(): Promise<string> {
     try {
       // Make the GET request
