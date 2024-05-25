7555b673eb09bdfa62ab1c8103c347f39f35a24a
Amanda de Almeida <economiaamanda@gmail.com>
2024-05-25 19:58:46 -0300

    CSS - ajustes em "search"

--- src/frontendWEB/src/pages/Search/Search.tsx
+++ src/frontendWEB/src/pages/Search/Search.tsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { OrganizationCard, SearchBar } from "../../components";
-import { Grid } from "@mui/material";
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Button,
+  Typography,
+  Box,
+} from "@mui/material";
 
 function Search() {
   const [organizations, setOrganizations] = useState([]);
@@ -24,8 +33,31 @@ function Search() {
       <SearchBar />
       <Grid container spacing={2} mt={2}>
         {organizations.map(({ name, logoUrl, id }) => (
-          <Grid item xs={3}>
-            <OrganizationCard title={name} image={logoUrl} id={id} />
+          <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
+            <Card sx={{ maxWidth: 345, margin: 1, boxShadow: 5 }}>
+              <CardMedia
+                component="img"
+                height="200px" // Adjust the height as needed to fill the top part of the card
+                image={logoUrl}
+                alt={name}
+                sx={{
+                  width: "100%",
+                  height: "200px",
+                  objectFit: "fill",
+                  paddingBottom: "20%",
+                }}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {name}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant="outlined" size="small">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
           </Grid>
         ))}
       </Grid>
