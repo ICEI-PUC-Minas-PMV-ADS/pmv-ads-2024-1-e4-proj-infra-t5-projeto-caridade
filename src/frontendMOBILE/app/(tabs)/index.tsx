b09e4a0fe068d88700fa8389b91bb4f7967856b9
Felipe Ferreira <114882542+Felipeadf@users.noreply.github.com>
2024-06-23 01:45:43 -0300

    Tela Principal

--- src/frontendMOBILE/app/(tabs)/index.tsx
+++ src/frontendMOBILE/app/(tabs)/index.tsx
@@ -1,13 +1,21 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Button} from 'react-native';
+import { Link, Tabs } from 'expo-router';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Charity Finder</Text>
+      <Text style={styles.title}>Encontre projetos que te engajam!</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.text}>Buscador de projetos de caridade que tem como objetivo auxiliar na
+          divulgação e busca de informações relativas a instituições
+          filantrópicas em âmbito global.
+      </Text>
+      <Link href="/cadastro" asChild>
+      <Button title='Criar Conta' color={'#006E8C'} ></Button>
+      </Link>
+
       {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
@@ -20,7 +28,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 30,
+    color: '#004e63',
     fontWeight: 'bold',
   },
   separator: {
@@ -28,4 +37,13 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  text: {
+    fontSize: 20,
+    textAlign: 'justify',
+    color: 'gray',
+    padding: 40,
+  },
+  button: {
+    color: '#006E8C',
+  }
 });
