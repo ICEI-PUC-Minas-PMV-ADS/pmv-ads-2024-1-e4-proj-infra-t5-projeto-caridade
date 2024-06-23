89adbf1cf69650780baf759aebd5543c3e3be8ff
Felipe Ferreira <114882542+Felipeadf@users.noreply.github.com>
2024-06-22 23:59:45 -0300

    frontendMOBILE configuracao

---
+++ src/frontendMOBILE/app/(tabs)/_layout.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Entypo } from '@expo/vector-icons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Link, Tabs } from 'expo-router';
+import { Pressable } from 'react-native';
+
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof Entypo>['name'];
+  color: string;
+}) {
+  return <Entypo size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: useClientOnlyValue(false, true),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Charity Finder',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          headerRight: () => (
+            <Link href="/Login" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <Entypo
+                    name="login"
+                    size={25}
+                    color={Colors[colorScheme ?? 'light'].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="busca"
+        options={{
+          title: 'Busca Entidade',
+          tabBarIcon: ({ color }) => <FontAwesome name="search" color={color} size={25} />,
+        }}
+      />
+    </Tabs>
+  );
+}
