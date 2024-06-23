c6ffec3f44cd29e7e96e2e8885c9daa94b46a7b5
Vinicius Carneiro Gonçalves <105397334+viniciuscg@users.noreply.github.com>
2024-06-23 18:49:07 -0300

    commit crud user

--- src/frontendMOBILE/app/(tabs)/busca.tsx
+++ src/frontendMOBILE/app/(tabs)/busca.tsx
@@ -5,11 +5,61 @@ import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
   return (
+<<<<<<< Updated upstream
     <View style={styles.container}>
       <Text style={styles.title}>Busca Entidade</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       {/* <EditScreenInfo path="app/(tabs)/two.tsx" /> */}
     </View>
+=======
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Appbar.Header style={styles.header}>
+          <Appbar.Content title="LOGO" titleStyle={styles.logo} />
+        </Appbar.Header>
+        <View style={styles.searchSection}>
+          <TextInput
+            style={styles.input}
+            mode="outlined"
+            placeholder="Encontrar projetos"
+            value={termo}
+            onChangeText={setTermo}
+            theme={{ colors: { primary: "#006E8C" } }}
+          />
+          <RNPickerSelect
+            onValueChange={(value) => setPais(value)}
+            items={paises}
+            placeholder={{ label: "Países", value: null }}
+            style={pickerSelectStyles}
+            useNativeAndroidPickerStyle={false}
+            Icon={() => {
+              return null;
+            }}
+          />
+          <RNPickerSelect
+            onValueChange={(value) => setTema(value)}
+            items={temas}
+            placeholder={{ label: "Temas", value: null }}
+            style={pickerSelectStyles}
+            useNativeAndroidPickerStyle={false}
+            Icon={() => {
+              return null;
+            }}
+          />
+          <Button mode="contained">
+            Buscar
+          </Button>
+        </View>
+        <FlatList
+          data={aplicarFiltros()}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          contentContainerStyle={styles.list}
+        />
+      </View>
+    </PaperProvider>
+>>>>>>> Stashed changes
   );
 }
 
