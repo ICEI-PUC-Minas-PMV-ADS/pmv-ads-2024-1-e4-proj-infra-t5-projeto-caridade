a0b370a83649515d692d02dfd5cea976a267e245
Vinicius Carneiro Gonçalves <105397334+viniciuscg@users.noreply.github.com>
2024-06-23 22:09:25 -0300

    organization fixed

--- src/frontendWEB/src/pages/Organization/Organization.tsx
+++ src/frontendWEB/src/pages/Organization/Organization.tsx
@@ -1,34 +1,65 @@
 import { Box, Typography } from "@mui/material";
-import { useFetch, useQueryParams } from "../../hooks";
+import { useQueryParams } from "../../hooks";
+import { organization } from "../../services/api";
+import { useEffect, useState } from "react";
+
+interface Organization {
+  organization: {
+    activeProjects: number;
+    addressLine1: string;
+    addressLine2: string;
+    city: string;
+    countries: { country: string[] };
+    country: string;
+    ein: string;
+    id: number;
+    iso3166CountryCode: string;
+    logoUrl: string;
+    mission: string;
+    name: string;
+    postal: string;
+    state: string;
+    themes: { theme: string[] };
+    totalProjects: number;
+    url: string;
+  }
+}
+
 
 function Organization() {
+  const [data, setData] = useState<Organization>();
   const params = useQueryParams();
   const organizationId = params.get("organization_id");
 
-  const { data: organizationData } = useFetch({
-    url: `/get-by-id/${organizationId}`,
-  });
+  const getOrganization = async () => {
+    const response = await organization.get<Organization>(`/get-by-id/${organizationId!}`);
+    console.log(response);
+    setData(response.data)
+  }
 
-  if (!organizationData) return <>loading...</>;
+  useEffect(() => {
+    getOrganization()
+  },[])
+  
 
   return (
     <Box display="flex">
       <Box alignItems="center" display="flex" height="100%" gap={5}>
         <div>
           <Typography variant="h3" color="#004e63">
-            {organizationData.organization.name}
+            {data?.organization.name}
           </Typography>
           <Box>
             <img
               width="400px"
-              src={organizationData.organization.logoUrl}
+              src={data?.organization.logoUrl}
               alt="Projeto1"
               style={{ borderRadius: "10px" }}
             />
           </Box>
         </div>
         <Typography variant="h6" mt={2} align="justify">
-          {organizationData.organization.mission}
+          {data?.organization.mission}
         </Typography>
       </Box>
     </Box>
